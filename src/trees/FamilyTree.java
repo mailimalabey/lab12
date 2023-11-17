c6b0865bdfefb911dfcf66252bac849294b595e3
mailimalabey <mai.malabey225@gmail.com>
2023-11-17 13:10:09 -0800

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -32,7 +32,9 @@ public class FamilyTree
         void addChild(TreeNode childNode)
         {
             // Add childNode to this node's children list. Also
-            // set childNode's parent to this node.
+        	children.add(childNode);
+        	// set childNode's parent to this node.
+        	childNode.parent = this;
         }
         
         
@@ -41,7 +43,7 @@ public class FamilyTree
         TreeNode getNodeWithName(String targetName)
         {
             // Does this node have the target name?
-            if (?????)
+            if (this.name.equals(targetName))
                 return this;
                     
             // No, recurse. Check all children of this node.
@@ -49,6 +51,8 @@ public class FamilyTree
             {
                 // If child.getNodeWithName(targetName) returns a non-null node,
                 // then that's the node we're looking for. Return it.
+            	if(child.getNodeWithName(targetName) != null)
+            		return child.getNodeWithName(targetName);
             }
             
             // Not found anywhere.
@@ -66,6 +70,13 @@ public class FamilyTree
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
+            TreeNode curr = null; 
+            
+            while (curr.parent!=null)
+            {
+            	ancestors.add(curr);
+            	curr = curr.parent;
+            }
 
             return ancestors;
         }
@@ -109,8 +120,8 @@ public class FamilyTree
 
 		// Parse the input file. Create a FileReader that reads treeFile. Create a BufferedReader
 		// that reads from the FileReader.
-		FileReader fr = ???
-		BufferedReader br = ???
+		FileReader fr = new FileReader(treeFile);
+		BufferedReader br = new BufferedReader(fr);
 		String line;
 		while ((line = br.readLine()) != null)
 			addLine(line);
